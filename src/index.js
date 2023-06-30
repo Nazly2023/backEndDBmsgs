3d2406377274c549cd8ec0885b2c545a0cf81c52
Nazly Zambrano <nazmi.016@gmail.com>
2023-06-30 15:50:28 -0500

    save bd

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(`${__dirname}/data/db.json`);
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
+
+const port = process.env.PORT || 3000;
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+server.use(router);
+server.use(cors());
+server.use(middlewares);
+
+server.listen(port, () => {
+  console.log("JSON Server is running");
+});
